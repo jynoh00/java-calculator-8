358fe9fc82b413db4343e3028ee7a2f384e9bc47
jynoh00 <wndus123sh@naver.com>
2025-10-19 04:15:45 +0900

    fix(calculator): correct typos in code

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -23,7 +23,7 @@ public class Application {
     }
 
     private static String readInput(){
-        System.out.println("덧셈할 문자열을 입력해주세요.");
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
     }
 
